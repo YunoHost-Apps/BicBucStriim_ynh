d37f9a14af4d0af3223a6b082ec08270e132dea4
ericgaspar <junk.eg@free.fr>
2021-07-07 12:16:50 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,8 @@
 
 YNH_PHP_VERSION="7.3"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mcrypt php${YNH_PHP_VERSION}-mbstring"
+extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
+#php${YNH_PHP_VERSION}-mcrypt
 
 #=================================================
 # PERSONAL HELPERS
