4b02b0b1f59f2ff9c6e07246a8753dcabe97f105
ericgaspar <junk.eg@free.fr>
2021-11-28 10:04:07 +0100

    Upgrade to 4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="7.3"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
+pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
 #php${YNH_PHP_VERSION}-mcrypt
 
 #=================================================
