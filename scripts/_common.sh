caba8afdc13aab6138a2ca96ce6012bcdb193b15
ericgaspar <junk.eg@free.fr>
2022-01-14 15:25:24 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,8 +6,7 @@
 
 YNH_PHP_VERSION="7.3"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
-#php${YNH_PHP_VERSION}-mcrypt
+pkg_dependencies="php${YNH_PHP_VERSION}-mcrypt php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
