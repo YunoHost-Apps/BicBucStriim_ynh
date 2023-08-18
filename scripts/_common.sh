779540534f0da61456ccf65523ff9270b21017f2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-18 20:40:02 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.3"
-
-#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-mbstring"
-#php${YNH_PHP_VERSION}-mcrypt
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
